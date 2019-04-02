ce81d5ba90d3af31af053a072473efe77b7ba013
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-02 14:40:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ get '/newteam' do
 end 
 
 post '/team' do 
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
   erb :team
 end 
 
